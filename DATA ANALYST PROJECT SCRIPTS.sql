d87a15bc6f7779042d562b53c9dced90d3f23107
Kristen Reeds <ksreeds2016@gmail.com>
2023-01-31 15:51:42 -0600

    initial push

--- DATA ANALYST PROJECT SCRIPTS.sql
+++ DATA ANALYST PROJECT SCRIPTS.sql
@@ -88,3 +88,49 @@ GROUP BY company, review_count, star_rating
 ORDER BY star_rating desc
 
 -- answer: American Expres w/a avg rating of 4.19
+
+-- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+
+SELECT DISTINCT title
+FROM data_analyst_jobs
+WHERE title LIKE '%Analyst';
+
+-- answer: 1071 total job titles that contain Analyst
+-- without duplicate titles, there's only 320 total 
+
+--12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? 
+-- What word do these positions have in common?
+
+SELECT DISTINCT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst%'
+AND title NOT LIKE '%Analytics%'
+AND title NOT LIKE '%ANALYST%'
+AND title NOT LIKE '%analytics%'
+AND title NOT LIKE '%ANALYTICS%'
+AND title NOT LIKE '%analyst%'
+
+-- Answer: 4 jobs without Analyst, or Analytics in the title 
+-- The 4 jobs left all involve Tableau in the description 
+
+-- **BONUS:**
+-- You want to understand which jobs requiring SQL are hard to fill. 
+-- Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+-- Disregard any postings where the domain is NULL. 
+-- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+-- Which three industries are in the top 4 on this list? 
+-- How many jobs have been listed for more than 3 weeks for each of the top 4?
+
+SELECT domain, days_since_posting, skill
+FROM data_analyst_jobs
+WHERE (domain IS NOT null)
+AND (days_since_posting IS NOT null)
+AND (skill IS NOT null)
+AND skill = 'SQL'
+AND days_since_posting >21
+ORDER BY domain DESC
+
+-- answer: Top 3 in the top 4 on the list
+-- Transport & Freight, Real Estate, Internet & Software
+-- 11 jobs total > 3 weeks
+-- T&F has 1, Real Estate has 1, I&S has 1 listing, and CG&S has 2 Listings over 3 weeks
